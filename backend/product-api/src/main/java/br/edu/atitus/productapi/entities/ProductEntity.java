1d49cd7b73b3ce59ef27106bf59431802e610773
KAEL FUCHS ZATTI <1137819@ATITUS.LOCAL>
2026-09-04 21:10:57 -0300

    v2

---
+++ backend/product-api/src/main/java/br/edu/atitus/productapi/entities/ProductEntity.java
@@ -0,0 +1,74 @@
+package br.edu.atitus.productapi.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_product")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String brand;
+    private String model;
+    private String description;
+    private String currency;
+    private double price;
+    private String image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
